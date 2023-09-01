78785129de20edacabda9430e146c9472a63afb3
Ryan Clarke <ryanclarke999@gmail.com>
2023-09-01 13:46:28 +0100

    Add project files.

---
+++ Payrate prompter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PayPrompter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double payrate;
+
+
+            do
+            {
+                Console.WriteLine("please enter a vaild payrate");
+                payrate = double.Parse(Console.ReadLine());
+
+            } while (payrate < 5.65 || payrate > 49.99);
+
+            Console.WriteLine("The valid payrate that you entered is £{0}", payrate);
+        }
+    }
+}
